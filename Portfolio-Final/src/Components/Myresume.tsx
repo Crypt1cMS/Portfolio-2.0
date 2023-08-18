d8456ce9f606c557f937177914dacd03fe5015b4
Crypt1cMS <dylan.minaya12@gmail.com>
2023-08-18 11:03:31 -0400

    Removed my resume

--- Portfolio-Final/src/Components/Myresume.tsx
+++
@@ -1,9 +0,0 @@
-import Resume from '../public/file/CV - Dylan Minaya.pdf'
-
-function File () {
-    return (
-        <a href='../public/file/CV - Dylan Minaya.pdf' download={Resume}>Download My Resume</a>
-    )
-}
-
-export default File
